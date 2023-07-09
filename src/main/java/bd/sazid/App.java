ec456e242b7e5bca89aa7d4ff0f3267bfe0b3cf1
sazidahmednassir <sazidahmednassir@gmail.com>
2023-07-09 19:28:21 +0600

    first commit

---
+++ src/main/java/bd/sazid/App.java
@@ -0,0 +1,13 @@
+package bd.sazid;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/main/java/bd/sazid/firstjava.java
@@ -0,0 +1,20 @@
+package bd.sazid;
+
+import java.util.Scanner;
+
+import javax.print.DocFlavor.INPUT_STREAM;
+
+public class firstjava {
+	
+	  public static void main( String[] args )
+	    {
+	        System.out.println( "sazid is the Best" );
+	        System.out.println("10");
+	        Scanner inputforcode = new Scanner(System.in);
+	        System.out.println("plese enter you name:");
+	        String name=inputforcode.nextLine();
+	        
+	        System.out.println(name);
+	    }
+
+}
