048fbd7050128cb13c06b88673c94db75d7e9069
sazidahmednassir <sazidahmednassir@gmail.com>
2023-07-09 23:29:10 +0600

    add Operators

---
+++ src/test/java/bd/sazid/Condition.java
@@ -0,0 +1,16 @@
+package bd.sazid;
+
+public class Condition {
+	public static void main(String[] args) {
+		
+		int age=1;
+		
+		if(age>10) {
+			System.out.println("Larger");
+		}
+		else {
+			System.out.println("smaller");
+		}
+	}
+
+}
---
+++ src/test/java/bd/sazid/JavaOperators.java
@@ -0,0 +1,61 @@
+package bd.sazid;
+
+public class JavaOperators {
+
+	public static void main(String[] args) {
+		
+		int a=10;
+		int b=20;
+		
+		System.out.println(a+b);
+		System.out.println(b+a);
+		System.out.println(b-a);
+		System.out.println(b/a);
+		System.out.println(b%a);
+		System.out.println(a*b);
+		
+		System.out.println(a<b);
+		System.out.println(a>b);
+		System.out.println(a<=b);
+		System.out.println(a>=b);
+		System.out.println(a==b);
+		System.out.println(a!=b);
+		
+		
+		boolean x=true;
+		boolean y=false;
+		
+		System.out.println(x&&y);
+		System.out.println(x||y);
+		System.out.println(!x);
+		System.out.println(!y);
+		
+		
+		int c;
+		
+		c=a;
+		
+		System.out.println(c);
+		
+		c++;
+		
+		
+		System.out.println(c);
+		
+		c+=a;
+		
+		System.out.println(c);
+		
+        c-=a;
+		
+		System.out.println(c);
+		
+        c*=a;
+		
+		System.out.println(c);
+		
+		c/=a;
+			
+		System.out.println(c);
+	}
+}
