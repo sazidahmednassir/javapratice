293cbb5685ac1f7d7814a2947f0a8af6e5b7e842
sazidahmednassir <sazidahmednassir@gmail.com>
2023-07-10 00:20:33 +0600

    conditional

---
+++ src/main/java/bd/sazid/Condition.java
@@ -0,0 +1,51 @@
+package bd.sazid;
+
+public class Condition {
+	public static void main(String[] args) {
+		
+		int age=20;
+		
+		if(age<10) {
+			System.out.println("minor");
+		}
+		
+		else if(age>=20) {
+			System.out.println("You big");
+		}
+		else {
+			System.out.println("Not Eigibele");
+		}
+		int day = 4;
+		switch (day) {
+		  case 1:
+		    System.out.println("Monday");
+		    break;
+		  case 2:
+		    System.out.println("Tuesday");
+		    break;
+		  case 3:
+		    System.out.println("Wednesday");
+		    break;
+		  case 4:
+		    System.out.println("Thursday");
+		    break;
+		  case 5:
+		    System.out.println("Friday");
+		    break;
+		  case 6:
+		    System.out.println("Saturday");
+		    break;
+		  case 7:
+		    System.out.println("Sunday");
+		    break;
+		  default:
+			    System.out.println("There is no day");
+			} 
+		
+		
+		
+		}
+	
+	
+
+}
