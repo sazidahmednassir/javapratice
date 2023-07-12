53cfcfa3f942eea6af173f8edf7387182e2b0ecd
sazidahmednassir <sazidahmednassir@gmail.com>
2023-07-12 16:08:14 +0600

    added while loop pratice

---
+++ src/main/java/bd/sazid/WhileLoopPA.java
@@ -0,0 +1,28 @@
+package bd.sazid;
+
+public class WhileLoopPA {
+	public static void main(String[] args) {
+		
+		int i=1;
+		
+		while(i<=10){
+			
+			System.out.println(i);
+			i++;
+		}
+		
+		
+		
+		int j=2;
+		while(j<=10) {
+			
+			System.out.println(j);
+			j+=2;
+		}
+		
+		
+		
+	}
+
+}
+
