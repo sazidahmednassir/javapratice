0f1cbc0039bf87f64513ea3e6dfff3846bd2a5f3
sazidahmednassir <sazidahmednassir@gmail.com>
2023-07-12 16:22:31 +0600

    added do while loop pratice

---
+++ src/main/java/bd/sazid/DoWhileJa.java
@@ -0,0 +1,16 @@
+package bd.sazid;
+
+public class DoWhileJa {
+	
+	public static void main(String[] args) {
+		
+		int i=1;
+		
+		do {
+			System.out.println(i);
+			i+=3;
+		}
+		while(i<=10);
+	}
+
+}
